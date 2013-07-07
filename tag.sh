78b763e6246525bbec3a1f5c0b57b473345a7fd2
Sören König <soeren.koenig@zalando.de>
2013-07-07 02:29:13 +0200

    updated README.rst and metadata

--- tag.sh
+++ tag.sh
@@ -4,7 +4,7 @@ read -p "new version:" new_version
 sed -i -e "s/__version__.*/__version__=\"${new_version}\"/g" jiracli/__init__.py 
 git add jiracli/__init__.py
 git commit -m "updating version to ${new_version}"
-git tag -s $(python setup.py --version) -m "tagging version ${new_version}"
+git tag -s -m "tagging version ${new_version}" $(python setup.py --version)
 python setup.py build sdist bdist_egg upload
 
 
